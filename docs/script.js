0797818b15b6d009273b4e2ed384aba91cba2cfa
Caleb A. Collar <49846301+Mindstormer-0@users.noreply.github.com>
2021-12-04 12:57:54 -0600

    Optimized hidden feature and fixed horizontal scrolling.

--- docs/script.js
+++ docs/script.js
@@ -6,24 +6,32 @@ const c = canvas.getContext('2d');
 const NUM_STARS = 1450;
 var body = document.body, html = document.documentElement;
 var y = window.scrollY - 2346;
+var currentStroke = c.strokeStyle;
 var hyperIO = false;
-var updated = true;
+var init = false;
 var drawing = false;
 let speed = 0.04;
 let stars = [];
 
 //Get initial dimensions.
+canvas.height = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight) + 500;
 adjustSize();
 
 //Hyper button.
 document.getElementById("hyperButton").addEventListener("click", function (e) {
   var element, elements;
   hyperIO ^= true;
+  if(!init){
+    createStars();
+    init = true;
+  }
   if (!drawing) {
     draw();
     drawing = true;
   }
   if (hyperIO) {
+    element = document.getElementById("circleBg");
+    element.style.transform = "scale(200)";
     element = document.getElementById("contentPane");
     element.classList.add("darkBorder");
     elements = document.getElementsByTagName("blockquote");
@@ -36,25 +44,34 @@ document.getElementById("hyperButton").addEventListener("click", function (e) {
         element.classList.add("reveal");
         element = document.getElementById("contentPane");
         element.classList.add("hyper");
+        element = document.getElementById("circleBg");
+        element.style.transform = "scale(0)";
       }
-      adjustSize();
     }, 500);
+    canvas.height = 5150;
+    adjustSize();
   }
   if (!hyperIO) {
-    element = document.getElementById("starField");
-    element.classList.remove("reveal");
-    element = document.getElementById("contentPane");
-    element.classList.remove("hyper");
-    element.classList.remove("darkBorder");
-    elements = document.getElementsByTagName("blockquote");
-    for (var i = 0; i < elements.length; i++) {
-      elements[i].classList.remove("darkBorder");
-    }
+    element = document.getElementById("circleBg");
+    element.style.transform = "scale(200)";
+    setTimeout(() => {
+      element = document.getElementById("starField");
+      element.classList.remove("reveal");
+      element = document.getElementById("circleBg");
+      element.style.transform = "scale(0)";
+      element = document.getElementById("contentPane");
+      element.classList.remove("hyper");
+      element.classList.remove("darkBorder");
+      elements = document.getElementsByTagName("blockquote");
+      for (var i = 0; i < elements.length; i++) {
+        elements[i].classList.remove("darkBorder");
+      }
+    }, 150);
     drawing = false;
   }
 });
 
-//On window scroll color and speed changes.
+//On window scroll position => color and speed changes.
 window.addEventListener('scroll', (event) => {
   y = window.scrollY - 2260;
   var perspective = Math.abs(y / 2260);
@@ -65,18 +82,17 @@ window.addEventListener('scroll', (event) => {
   }
   c.strokeStyle = 'rgb(' + Math.round(255 - (21 * perspective)) + ', ' + Math.round(255 - (247 * perspective)) + ', ' + Math.round(255 - (199 * perspective)) + ')';
   currentStroke = c.strokeStyle;
-  //console.log(perspective);
-  //234, 8, 56
+  //234, 8, 56 ~ redshift color
 });
 
 //Star object.
 class Star {
   constructor() {
-    //Initialize star paths.
+    //Initialize the star paths.
     this.x = Math.random() * canvas.width - canvas.width / 2;  //random x
     this.y = Math.random() * canvas.height - canvas.height / 2; //random y
     this.px, this.py;
-    this.z = Math.random() * 4; //random z    
+    this.z = Math.random() * 4; //Random z    
   }
 
   update() {
@@ -108,23 +124,21 @@ class Star {
 }
 
 //Create stars.
-for (let i = 0; i < NUM_STARS; i++) stars.push(new Star());
-c.fillStyle = 'rgba(0, 0, 0, 0.1)';
-c.strokeStyle = 'rgb(255,255,255)';
-var currentStroke = c.strokeStyle;
-
-//c.translate(canvas.width / 2, canvas.height / 2);
+function createStars() {
+  for (let i = 0; i < NUM_STARS; i++) stars.push(new Star());
+  c.fillStyle = 'rgba(0, 0, 0, 0.1)';
+  c.strokeStyle = 'rgb(255,255,255)';
+  currentStroke = c.strokeStyle;
+}
 
 //Draw background and stars method.
 function draw() {
-  c.fillRect(-canvas.width / 2, -canvas.height / 2, canvas.width, canvas.height);
-  for (let s of stars) {
-    s.update();
-    s.show();
-  }
-  if (!hyperIO) {
-    cancelAnimationFrame(draw);
-  } else {
+  if (hyperIO) {
+    c.fillRect(-canvas.width / 2, -canvas.height / 2, canvas.width, canvas.height);
+    for (let s of stars) {
+      s.update();
+      s.show();
+    }
     requestAnimationFrame(draw);
   }
 }
@@ -132,7 +146,6 @@ function draw() {
 //Check canvas size against document size.
 function adjustSize() {
   canvas.width = window.innerWidth;
-  canvas.height = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight) + 175;
   c.translate(canvas.width / 2, canvas.height / 2);
   c.strokeStyle = currentStroke;
 }
